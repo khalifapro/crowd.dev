fc225a44dc68d9dc7be01161b2a97b16d089bc12
anilb <epipav@gmail.com>
2024-06-06 11:05:02 +0200

    add to workflows list

--- services/apps/script_executor_worker/src/workflows.ts
+++ services/apps/script_executor_worker/src/workflows.ts
@@ -1,9 +1,11 @@
 import { findAndMergeMembersWithSamePlatformIdentitiesDifferentCapitalization } from './workflows/findAndMergeMembersWithSamePlatformIdentitiesDifferentCapitalization'
 import { findAndMergeMembersWithSameVerifiedEmailsInDifferentPlatforms } from './workflows/findAndMergeMembersWithSameVerifiedEmailsInDifferentPlatforms'
 import { dissectMember } from './workflows/dissectMember'
+import { fixVerifiedIdentities } from './workflows/fixVerifiedIdentities'
 
 export {
   findAndMergeMembersWithSameVerifiedEmailsInDifferentPlatforms,
   findAndMergeMembersWithSamePlatformIdentitiesDifferentCapitalization,
   dissectMember,
+  fixVerifiedIdentities,
 }
